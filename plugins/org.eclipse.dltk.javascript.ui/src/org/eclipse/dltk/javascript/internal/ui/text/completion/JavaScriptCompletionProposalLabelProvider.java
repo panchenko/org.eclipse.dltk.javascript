2f9f8812fe3880f3761e5f6174e4e27e3dc0f335
asobolev <asobolev>
2008-04-21 10:46:14 +0000

    *fixes

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/completion/JavaScriptCompletionProposalLabelProvider.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/completion/JavaScriptCompletionProposalLabelProvider.java
@@ -43,7 +43,7 @@ public class JavaScriptCompletionProposalLabelProvider extends
 		// parameters
 		nameBuffer.append('(');
 		appendUnboundedParameterList(nameBuffer, methodProposal);
-		nameBuffer.append(")  "); //$NON-NLS-1$
+		nameBuffer.append(')'); //$NON-NLS-1$
 
 		return nameBuffer.toString();
 	}
