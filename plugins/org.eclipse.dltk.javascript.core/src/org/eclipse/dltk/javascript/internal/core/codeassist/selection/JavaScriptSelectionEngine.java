c258ac424fb782ae6c0f79f77ac78ae48ce6319e
jcompagne <jcompagne>
2008-04-28 15:09:50 +0000

    instanceof check fix (the iterator was checked for an instance instead of the object that was from the iterator) The position calculator should not just get to the offset (which could be the mouse position) but take into account the complete line. Else for example hoovers dont work correctly because the statement isnt complete.

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/internal/core/codeassist/selection/JavaScriptSelectionEngine.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/internal/core/codeassist/selection/JavaScriptSelectionEngine.java
@@ -34,6 +34,7 @@ import org.eclipse.dltk.core.search.SearchParticipant;
 import org.eclipse.dltk.core.search.SearchPattern;
 import org.eclipse.dltk.core.search.SearchRequestor;
 import org.eclipse.dltk.internal.javascript.reference.resolvers.ReferenceResolverContext;
+import org.eclipse.dltk.internal.javascript.typeinference.FakeField;
 import org.eclipse.dltk.internal.javascript.typeinference.HostCollection;
 import org.eclipse.dltk.internal.javascript.typeinference.IReference;
 import org.eclipse.dltk.internal.javascript.typeinference.VaribleDeclarationReference;
@@ -42,115 +43,129 @@ import org.eclipse.dltk.javascript.internal.core.codeassist.AssitUtils;
 
 public class JavaScriptSelectionEngine extends ScriptSelectionEngine {
 
-	public JavaScriptSelectionEngine(/* Map settings */) {
-		// super(settings);
+	public JavaScriptSelectionEngine(/*Map settings*/) {
+//		super(settings);
 	}
 
 	public IAssistParser getParser() {
 		return null;
 	}
-
+	
 	ISourceModule cu;
 
 	public IModelElement[] select(ISourceModule cu, int offset, int i) {
 		String content = cu.getSourceContents();
 		char[] fileName = cu.getFileName();
-		this.cu = cu;
+		this.cu=cu;
+		int index = content.indexOf('\n', offset);
+		if (index == -1)
+		{
+			index = content.length();
+		}
 		ReferenceResolverContext buildContext2 = AssitUtils.buildContext(
-				(org.eclipse.dltk.core.ISourceModule) cu, offset, content,
+				(org.eclipse.dltk.core.ISourceModule) cu, index, content,
 				fileName);
 		HostCollection buildContext = buildContext2.getHostCollection();
 		AssitUtils.PositionCalculator calc = new AssitUtils.PositionCalculator(
 				content, offset, true);
 		// if (i==offset)i=1;
-		final List result = new ArrayList();
+		final List result=new ArrayList();
 		String selection = calc.getCompletion();
 		if (calc.isMember()) {
 			processMember(buildContext2, calc, result, selection);
 		} else {
 			processGlobals(buildContext2, buildContext, result, selection);
 		}
-		HashSet sm = new HashSet(result);
-		IModelElement[] resultA = new IModelElement[sm.size()];
+		HashSet sm=new HashSet(result);
+		IModelElement[] resultA=new IModelElement[sm.size()];
 		sm.toArray(resultA);
+		for (int j = 0; j < resultA.length; j++)
+		{
+			if (resultA[j] instanceof IField)
+			{
+				((IField)resultA[j]).setSnippet(selection);
+			}
+		}
 		return resultA;
 	}
 
 	private void processGlobals(ReferenceResolverContext rc,
 			HostCollection buildContext, final List result, String selection) {
 		if (!(selection.length() == 0)) {
-			// local defenitition
-			// global member;
-			IReference rm = buildContext.getReference(selection);
+		//local defenitition
+		//global member;					
+		IReference rm= buildContext.getReference(selection);
 			if (rm != null) {
-				rm.addModelElements(result);
+			rm.addModelElements(result);
 			} else {
-				Set resolveGlobals = rc.resolveGlobals(selection);
-				Iterator it = resolveGlobals.iterator();
-				while (it.hasNext()) {
-					Object next = it.next();
-					if (it instanceof IReference) {
-						IReference r = (IReference) next;
-						if (r.getName().equals(selection))
-							r.addModelElements(result);
-					}
+			Set resolveGlobals = rc.resolveGlobals(selection);
+			Iterator it=resolveGlobals.iterator();
+			while (it.hasNext()){
+				Object next = it.next();
+				if (next instanceof IReference)
+				{
+				IReference r=(IReference) next;
+				if (r.getName().equals(selection))
+				r.addModelElements(result);
 				}
 			}
-
+		}
+		
 			if (result.size() == 0) {
-				doCompletionOnFunction(selection, result);
-				doCompletionOnGlobalVariable(selection, result);
-			}
+		doCompletionOnFunction(selection, result);
+		doCompletionOnGlobalVariable(selection,result);
+		}
 		}
 	}
 
 	private void processMember(ReferenceResolverContext buildContext,
 			AssitUtils.PositionCalculator calc, final List result,
 			String selection) {
-		String core = calc.getCorePart();
+		String core=calc.getCorePart();
 		IReference rm = buildContext.getHostCollection().queryElement(
 				selection, true);
-		if (rm != null)
-			rm.addModelElements(result);
+		if (rm!=null)
+		rm.addModelElements(result);
 		if (result.size() == 0) {
-			IDLTKLanguageToolkit toolkit = null;
-			toolkit = DLTKLanguageManager
-					.getLanguageToolkit(JavaScriptNature.NATURE_ID);
-			Set resolveGlobals = buildContext.resolveGlobals(selection);
-			Iterator it = resolveGlobals.iterator();
-			while (it.hasNext()) {
-				IReference r = (IReference) it.next();
-				r.addModelElements(result);
-			}
+		IDLTKLanguageToolkit toolkit = null;
+		toolkit = DLTKLanguageManager
+				.getLanguageToolkit(JavaScriptNature.NATURE_ID);
+		Set resolveGlobals = buildContext.resolveGlobals(selection);
+		Iterator it=resolveGlobals.iterator();
+		while (it.hasNext()){
+			IReference r=(IReference) it.next();
+			r.addModelElements(result);
+		}
+		
+		SearchRequestor requestor = new SearchRequestor() {
 
-			SearchRequestor requestor = new SearchRequestor() {
-
-				public void acceptSearchMatch(SearchMatch match)
-						throws CoreException {
-					FieldReferenceMatch mr = (FieldReferenceMatch) match;
-					ASTNode nm = mr.getNode();
-					if (nm instanceof VaribleDeclarationReference) {
-						VaribleDeclarationReference vm = (VaribleDeclarationReference) nm;
-						IReference reference = vm.getReference();
-						if (reference != null) {
-							reference.addModelElements(result);
-						}
+			public void acceptSearchMatch(SearchMatch match)
+					throws CoreException {
+				FieldReferenceMatch mr = (FieldReferenceMatch) match;
+				ASTNode nm=mr.getNode();
+				if (nm instanceof VaribleDeclarationReference){
+					VaribleDeclarationReference vm=(VaribleDeclarationReference) nm;
+					IReference reference = vm.getReference();
+					if (reference!=null){
+						reference.addModelElements(result);
 					}
-
 				}
-
-			};
-			IDLTKSearchScope scope = SearchEngine.createWorkspaceScope(toolkit);
-			try {
-
-				search(selection, IDLTKSearchConstants.FIELD,
-						IDLTKSearchConstants.REFERENCES, scope, requestor);
-			} catch (CoreException e) {
-				e.printStackTrace();
+				
+				
 			}
+
+		};
+		IDLTKSearchScope scope = SearchEngine.createWorkspaceScope(toolkit);
+		try {
+
+			search(selection , IDLTKSearchConstants.FIELD,
+					IDLTKSearchConstants.REFERENCES, scope, requestor);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
 		}
 	}
-
+		
 	private void doCompletionOnFunction(final String startPart,
 			final List modelElements) {
 		SearchRequestor requestor = new SearchRequestor() {
@@ -158,11 +173,11 @@ public class JavaScriptSelectionEngine extends ScriptSelectionEngine {
 					throws CoreException {
 				Object element = match.getElement();
 				if (element instanceof IMethod) {
-					IMethod mn = (IMethod) element;
+					IMethod mn=(IMethod) element;
 					if (mn.getElementName().equals(startPart)) {
 						if (!modelElements.isEmpty())
 							return;
-						modelElements.add(element);
+					modelElements.add(element);
 					}
 				}
 			}
@@ -177,9 +192,9 @@ public class JavaScriptSelectionEngine extends ScriptSelectionEngine {
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}		
 	}
-
+	
 	private void doCompletionOnGlobalVariable(String startPart,
 			final List methods) {
 		SearchRequestor requestor = new SearchRequestor() {
@@ -187,20 +202,20 @@ public class JavaScriptSelectionEngine extends ScriptSelectionEngine {
 					throws CoreException {
 				Object element = match.getElement();
 				if (element instanceof IField) {
-					methods.add(element);
+					methods.add(element);					
 				}
 				if (match instanceof FieldReferenceMatch) {
 					FieldReferenceMatch mr = (FieldReferenceMatch) match;
-
-					// String string = mr.getNode().toString();
-					// if (string.startsWith("!!!"))
-					// return;
-					// int i = string.indexOf('.');
-					// if (i != -1)
-					// string = string.substring(0, i);
-					// if (!completedNames.contains(string))
-					// props.add(string);
-
+					
+					//String string = mr.getNode().toString();
+					//if (string.startsWith("!!!"))
+					//	return;
+					//int i = string.indexOf('.');
+					//if (i != -1)
+					//	string = string.substring(0, i);
+					//if (!completedNames.contains(string))
+					//props.add(string);
+					
 				}
 			}
 		};
@@ -216,9 +231,9 @@ public class JavaScriptSelectionEngine extends ScriptSelectionEngine {
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}				
 	}
-
+	
 	protected void search(String patternString, int searchFor, int limitTo,
 			IDLTKSearchScope scope, SearchRequestor resultCollector)
 			throws CoreException {
@@ -235,7 +250,7 @@ public class JavaScriptSelectionEngine extends ScriptSelectionEngine {
 		}
 		SearchPattern pattern = SearchPattern.createPattern(patternString,
 				searchFor, limitTo, matchRule, scope.getLanguageToolkit());
-		if (pattern == null) {
+		if (pattern==null){
 			pattern = SearchPattern.createPattern(patternString, searchFor,
 					limitTo, matchRule, scope.getLanguageToolkit());
 		}
@@ -244,5 +259,6 @@ public class JavaScriptSelectionEngine extends ScriptSelectionEngine {
 						.getDefaultSearchParticipant() }, scope, requestor,
 				null);
 	}
+	
 
 }
