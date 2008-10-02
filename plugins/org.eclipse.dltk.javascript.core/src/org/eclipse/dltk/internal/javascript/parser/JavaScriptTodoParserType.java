80059ea912c2afd7d312cfad9bc478ca51c6b2c0
apanchenk <apanchenk>
2008-10-02 10:34:24 +0000

    move builder to the core

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/JavaScriptTodoParserType.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/JavaScriptTodoParserType.java
@@ -1,9 +1,9 @@
 package org.eclipse.dltk.internal.javascript.parser;
 
 import org.eclipse.core.runtime.Preferences;
+import org.eclipse.dltk.core.builder.AbstractTodoTaskBuildParticipantType;
 import org.eclipse.dltk.javascript.core.JavaScriptNature;
 import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
-import org.eclipse.dltk.validators.core.AbstractTodoTaskBuildParticipantType;
 
 public class JavaScriptTodoParserType extends
 		AbstractTodoTaskBuildParticipantType {
