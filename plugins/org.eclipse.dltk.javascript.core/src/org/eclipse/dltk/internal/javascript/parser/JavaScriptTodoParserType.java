2d46fbd376c884b1c874d6559b36a192c739a3e7
apanchenk <apanchenk>
2008-10-03 13:25:14 +0000

    major builder infrastructure refactoring

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/JavaScriptTodoParserType.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/JavaScriptTodoParserType.java
@@ -1,25 +1,17 @@
 package org.eclipse.dltk.internal.javascript.parser;
 
-import org.eclipse.core.runtime.Preferences;
+import org.eclipse.dltk.compiler.task.ITodoTaskPreferences;
+import org.eclipse.dltk.compiler.task.TodoTaskPreferences;
+import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.builder.AbstractTodoTaskBuildParticipantType;
-import org.eclipse.dltk.javascript.core.JavaScriptNature;
 import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
 
 public class JavaScriptTodoParserType extends
 		AbstractTodoTaskBuildParticipantType {
 
-	private static final String ID = "org.eclipse.dltk.javascript.todo"; //$NON-NLS-1$
-	private static final String NAME = "JavaScript TODO task parser"; //$NON-NLS-1$
-
-	public JavaScriptTodoParserType() {
-		super(ID, NAME);
+	protected ITodoTaskPreferences getPreferences(IScriptProject project) {
+		return new TodoTaskPreferences(JavaScriptPlugin.getDefault()
+				.getPluginPreferences());
 	}
 
-	protected Preferences getPreferences() {
-		return JavaScriptPlugin.getDefault().getPluginPreferences();
-	}
-
-	public String getNature() {
-		return JavaScriptNature.NATURE_ID;
-	}
 }
