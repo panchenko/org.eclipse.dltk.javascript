0cfdfe030f2291a383d8883046ee0d6be7da40e6
jcompagne <jcompagne>
2008-10-04 14:48:06 +0000

    javascript task/fixme parsing

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/JavaScriptTodoParserType.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/JavaScriptTodoParserType.java
@@ -4,6 +4,7 @@ import org.eclipse.dltk.compiler.task.ITodoTaskPreferences;
 import org.eclipse.dltk.compiler.task.TodoTaskPreferences;
 import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.builder.AbstractTodoTaskBuildParticipantType;
+import org.eclipse.dltk.core.builder.IBuildParticipant;
 import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
 
 public class JavaScriptTodoParserType extends
@@ -14,4 +15,28 @@ public class JavaScriptTodoParserType extends
 				.getPluginPreferences());
 	}
 
+	protected IBuildParticipant getBuildParticipant(
+			ITodoTaskPreferences preferences) {
+		return new TodoTaskBuildParticipant(preferences) {
+			protected int findCommentStart(char[] content, int begin, int end) {
+				if (!isCheckRanges()) {
+					begin = skipSpaces(content, begin, end);
+					if (begin + 1 < end
+							&& content[begin] == '/'
+							&& (content[begin + 1] == '/' || content[begin + 1] == '*')) {
+						return begin + 2;
+					}
+					return -1;
+				}
+				for (int i = begin; i < end; ++i) {
+					if (content[i] == '/'
+							&& (i + 1 < end && (content[i + 1] == '/' || content[i + 1] == '*'))
+							&& isValid(i)) {
+						return i + 2;
+					}
+				}
+				return -1;
+			}
+		};
+	}
 }
