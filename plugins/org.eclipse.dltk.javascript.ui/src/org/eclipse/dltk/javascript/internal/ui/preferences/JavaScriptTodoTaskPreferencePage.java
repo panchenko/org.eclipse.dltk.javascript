76ca6a7225cc3e492997be4c4fc7466167e2da62
jgangemi <jgangemi>
2008-11-02 18:36:38 +0000

    - support for project specific task tags

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavaScriptTodoTaskPreferencePage.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavaScriptTodoTaskPreferencePage.java
@@ -1,15 +1,27 @@
 package org.eclipse.dltk.javascript.internal.ui.preferences;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.Preferences;
 import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
-import org.eclipse.dltk.ui.preferences.TodoTaskAbstractPreferencePage;
+import org.eclipse.dltk.ui.PreferencesAdapter;
+import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage;
+import org.eclipse.dltk.ui.preferences.AbstractOptionsBlock;
+import org.eclipse.dltk.ui.preferences.AbstractTodoTaskOptionsBlock;
+import org.eclipse.dltk.ui.preferences.PreferenceKey;
+import org.eclipse.dltk.ui.util.IStatusChangeListener;
+import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 
 public class JavaScriptTodoTaskPreferencePage extends
-		TodoTaskAbstractPreferencePage {
+		AbstractConfigurationBlockPropertyAndPreferencePage {
 
-	protected Preferences getPluginPreferences() {
-		return JavaScriptPlugin.getDefault().getPluginPreferences();
-	}
+	static final PreferenceKey CASE_SENSITIVE = AbstractTodoTaskOptionsBlock
+			.createCaseSensitiveKey(JavaScriptPlugin.PLUGIN_ID);
+
+	static final PreferenceKey ENABLED = AbstractTodoTaskOptionsBlock
+			.createEnabledKey(JavaScriptPlugin.PLUGIN_ID);
+
+	static final PreferenceKey TAGS = AbstractTodoTaskOptionsBlock
+			.createTagKey(JavaScriptPlugin.PLUGIN_ID);
 
 	protected String getHelpId() {
 		return null;
@@ -17,5 +29,49 @@ public class JavaScriptTodoTaskPreferencePage extends
 
 	protected void setDescription() {
 		setDescription(JavaScriptPreferenceMessages.TodoTaskDescription);
+	}
+
+	protected Preferences getPluginPreferences() {
+		return JavaScriptPlugin.getDefault().getPluginPreferences();
+	}
+
+	protected AbstractOptionsBlock createOptionsBlock(
+			IStatusChangeListener newStatusChangedListener, IProject project,
+			IWorkbenchPreferenceContainer container) {
+		return new AbstractTodoTaskOptionsBlock(newStatusChangedListener,
+				project, getPreferenceKeys(), container) {
+			protected PreferenceKey getTags() {
+				return TAGS;
+			}
+
+			protected PreferenceKey getEnabledKey() {
+				return ENABLED;
+			}
+
+			protected PreferenceKey getCaseSensitiveKey() {
+				return CASE_SENSITIVE;
+			}
+		};
+	}
+
+	protected String getProjectHelpId() {
+		return null;
+	}
+
+	protected void setPreferenceStore() {
+		setPreferenceStore(new PreferencesAdapter(JavaScriptPlugin.getDefault()
+				.getPluginPreferences()));
+	}
+
+	protected String getPreferencePageId() {
+		return "org.eclipse.dltk.javascript.preferences.todo";
+	}
+
+	protected String getPropertyPageId() {
+		return "org.eclipse.dltk.javascript.propertyPage.todo";
+	}
+
+	protected PreferenceKey[] getPreferenceKeys() {
+		return new PreferenceKey[] { TAGS, ENABLED, CASE_SENSITIVE };
 	}
 }
