682cfb5b9e121179d5c4e6cb5746eadd0d8b5ae6
apanchenk <apanchenk>
2008-11-08 13:12:24 +0000

    Eclipse 3.3 compatibility - use copy of FileLocator.getBundleFile(Bundle)

--- plugins/org.eclipse.dltk.javascript.launching/src/org/eclipse/dltk/javascript/internal/launching/ClasspathUtils.java
+++ plugins/org.eclipse.dltk.javascript.launching/src/org/eclipse/dltk/javascript/internal/launching/ClasspathUtils.java
@@ -2,6 +2,7 @@ package org.eclipse.dltk.javascript.internal.launching;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
@@ -38,12 +39,41 @@ public class ClasspathUtils {
 	private static File getPluginLocation(String bundleId) throws CoreException {
 		try {
 			final Bundle bundle = Platform.getBundle(bundleId);
-			return FileLocator.getBundleFile(bundle);
+			return getBundleFile(bundle);
 		} catch (IOException e) {
 			final String msg = NLS.bind(PLUGIN_LOCATION_ERROR, bundleId);
 			throw new CoreException(new Status(IStatus.ERROR,
 					JavaScriptLaunchingPlugin.PLUGIN_ID, IStatus.ERROR, msg, e));
 		}
+	}
+
+	/**
+	 * Returns a file for the contents of the specified bundle. Depending on how
+	 * the bundle is installed the returned file may be a directory or a jar
+	 * file containing the bundle content.
+	 * 
+	 * XXX 3.3 compatibility = {@link FileLocator#getBundleFile(Bundle)}
+	 * 
+	 * @param bundle
+	 *            the bundle
+	 * @return a file with the contents of the bundle
+	 * @throws IOException
+	 *             if an error occurs during the resolution
+	 */
+	public static File getBundleFile(Bundle bundle) throws IOException {
+		URL rootEntry = bundle.getEntry("/"); //$NON-NLS-1$
+		rootEntry = FileLocator.resolve(rootEntry);
+		if ("file".equals(rootEntry.getProtocol())) //$NON-NLS-1$
+			return new File(rootEntry.getPath());
+		if ("jar".equals(rootEntry.getProtocol())) { //$NON-NLS-1$
+			String path = rootEntry.getPath();
+			if (path.startsWith("file:")) {
+				// strip off the file: and the !/
+				path = path.substring(5, path.length() - 2);
+				return new File(path);
+			}
+		}
+		throw new IOException("Unknown protocol"); //$NON-NLS-1$
 	}
 
 	private static final String PLUGIN_LOCATION_ERROR = "Error determining classpath from bundle {0}"; //$NON-NLS-1$
