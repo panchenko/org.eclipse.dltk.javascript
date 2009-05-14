0445e938dff662dd8dd3f032fef5dca63333f759
apanchenk <apanchenk>
2009-05-14 02:13:05 +0000

    s/tcl/javascript/

--- plugins/org.eclipse.dltk.javascript.debug.ui/src/org/eclipse/dltk/javascript/internal/console/ui/JavaScriptConsole.java
+++ plugins/org.eclipse.dltk.javascript.debug.ui/src/org/eclipse/dltk/javascript/internal/console/ui/JavaScriptConsole.java
@@ -4,15 +4,16 @@ import org.eclipse.dltk.console.ui.ScriptConsole;
 import org.eclipse.dltk.javascript.console.JavaScriptInterpreter;
 
 public class JavaScriptConsole extends ScriptConsole {
-	public static final String CONSOLE_TYPE = "tcl_console";
+	public static final String CONSOLE_TYPE = "javascript_console";
 
-	public static final String CONSOLE_NAME = "Tcl Console";
-	
+	public static final String CONSOLE_NAME = "JavaScript Console";
+
 	public JavaScriptConsole(JavaScriptInterpreter interpreter, String id) {
 		super(CONSOLE_NAME + " [" + id + "]", CONSOLE_TYPE);
 
 		setInterpreter(interpreter);
 		setTextHover(new JavaScriptConsoleTextHover(interpreter));
-		setContentAssistProcessor(new JavaScriptConsoleCompletionProcessor(interpreter));
-	}	
+		setContentAssistProcessor(new JavaScriptConsoleCompletionProcessor(
+				interpreter));
+	}
 }
