70d3309dc3ef650f612085016561f9f5ef228aa1
apanchenk <apanchenk>
2009-06-12 15:44:53 +0000

    trim

--- tests/org.eclipse.dltk.javascript.formatter.tests/scripts.parser/e4x.js
+++ tests/org.eclipse.dltk.javascript.formatter.tests/scripts.parser/e4x.js
@@ -1,15 +1,15 @@
 
 default xml namespace = "http://www.w3.org/1999/xhtml";
 
-var books = 
+var books =
 	<books>
 		<authors>
 			<author name = "Alexandre Dumas">
-				Alexandre Dumas est un écrivain français, né le 24 juillet 1802 (5 thermidor an X) 
-				à Villers-Cotterêts (Aisne) et mort le 5 décembre 1870 à Puys, près de Dieppe 
-				(Seine-Maritime). Il est le fils du général Thomas Alexandre Dumas et le père 
-				de l''écrivain Alexandre Dumas (1824-1895) dit Dumas fils, 
-				auteur en particulier de La Dame aux camélias.			
+				Alexandre Dumas est un écrivain français, né le 24 juillet 1802 (5 thermidor an X)
+				à Villers-Cotterêts (Aisne) et mort le 5 décembre 1870 à Puys, près de Dieppe
+				(Seine-Maritime). Il est le fils du général Thomas Alexandre Dumas et le père
+				de l''écrivain Alexandre Dumas (1824-1895) dit Dumas fils,
+				auteur en particulier de La Dame aux camélias.
 				александр Дюма
 			</author>
 		</authors>
@@ -21,7 +21,7 @@ var language =
 
 
 var languages2 = 
- <?xml version='1.0' encoding='utf-8'?>  
+ <?xml version='1.0' encoding='utf-8'?>
  <languages type="dynamic">
  	<!-- sdfdgdfgh -->
    <lang default = true>JavaScript</lang>
