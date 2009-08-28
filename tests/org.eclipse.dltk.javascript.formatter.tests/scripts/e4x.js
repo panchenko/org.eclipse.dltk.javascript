67a63e9b0cab5ec83e3fa0cea83db3a8fccbd5cd
apanchenk <apanchenk>
2009-08-28 08:38:15 +0000

    test for xml variable

--- tests/org.eclipse.dltk.javascript.formatter.tests/scripts/e4x.js
+++ tests/org.eclipse.dltk.javascript.formatter.tests/scripts/e4x.js
@@ -1,3 +1,7 @@
+==== ex4-simple-xml-variable
+	var xml =	 		<language>JavaScript</language>;
+==
+var xml = <language>JavaScript</language>;
 ==== ex4-simple
 	var xml2 =
 	 		<language>JavaScript</language>;
