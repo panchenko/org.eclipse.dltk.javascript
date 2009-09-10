32f886edc0b8b23770718f11fe3f46a5f38f553f
jcompagne <jcompagne>
2009-09-10 14:58:56 +0000

    added some text (1 is failing at the moment)

--- tests/org.eclipse.dltk.javascript.formatter.tests/scripts/e4x.js
+++ tests/org.eclipse.dltk.javascript.formatter.tests/scripts/e4x.js
@@ -8,4 +8,39 @@ var xml = <language>JavaScript</language>;
 ==
 var xml2 =
 	<language>JavaScript</language>;
-====	 		
+==== keyword open and a close tag
+    function test3()
+    {
+	    var _xml =      <xml></<xml>;
+    }
+==
+function test3 () {
+	var _xml = <xml></<xml>;
+}
+==== keyword openclose tag
+    function test3()
+    {
+	    var _xml =      <xml/>;
+    }
+==
+function test3()
+{
+	var _xml =      <xml/>;
+}
+==== none keyword openclose tag
+    function test3()
+    {
+	    var _xml =      <xml2/>;
+    }
+==
+function test3 () {
+	var _xml = <xml2/>;
+}
+==== xml variable usage
+	function XMLRPC_request(type, value) {
+    	xml = <{type}>value</{type}>;
+	}
+==
+function XMLRPC_request (type, value) {
+	xml = <{type}>value</{type}>;
+}
