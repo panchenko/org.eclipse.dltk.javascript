f059dccd95dee884ed93e8b8c993fd6e876ed4cc
apanchenk <apanchenk>
2009-10-13 18:00:36 +0000

    test for omitted values in array initialization

--- tests/org.eclipse.dltk.javascript.formatter.tests/scripts/arrays.js
+++ tests/org.eclipse.dltk.javascript.formatter.tests/scripts/arrays.js
@@ -2,4 +2,8 @@
 	dataSource.addRow (		100			,		 [		200				,	3000		]	);
 ==
 dataSource.addRow ( 100, [200, 3000] );
+==== array-omitted-values
+var vArray  = [1      ,0            ,,]
+==
+var vArray = [1, 0, , ]
 ====
