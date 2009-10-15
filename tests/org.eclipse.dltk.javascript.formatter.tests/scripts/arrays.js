879e1cb1b69b78a982422ec8a8b5c26a4dbc0dce
apanchenk <apanchenk>
2009-10-15 08:23:55 +0000

    tests for array formatting

--- tests/org.eclipse.dltk.javascript.formatter.tests/scripts/arrays.js
+++ tests/org.eclipse.dltk.javascript.formatter.tests/scripts/arrays.js
@@ -1,9 +1,13 @@
 ==== array-simple
 	dataSource.addRow (		100			,		 [		200				,	3000		]	);
 ==
-dataSource.addRow ( 100, [200, 3000] );
-==== array-omitted-values
-var vArray  = [1      ,0            ,,]
+dataSource.addRow(100, [200, 3000]);
+==== array-omitted-values-1
+var vArray  = [   1      ,2            ,,             ]
 ==
-var vArray = [1, 0, , ]
+var vArray = [1, 2, , ]
+==== array-omitted-values-2
+var vArray  = [    1      ,2            ,,,    3            ]
+==
+var vArray = [1, 2, , , 3]
 ====
