edbf2b36c8752e4876b3b68d4d41e229e66a0608
apanchenk <apanchenk>
2010-01-22 02:37:06 +0000

    new interface introduced

---
+++ plugins/org.eclipse.dltk.javascript.parser/src/org/eclipse/dltk/javascript/parser/JSTokenStream.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2010 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.javascript.parser;
+
+import java.util.List;
+
+import org.antlr.runtime.Token;
+import org.antlr.runtime.TokenStream;
+
+public interface JSTokenStream extends TokenStream {
+
+	List<Token> getTokens();
+
+}
