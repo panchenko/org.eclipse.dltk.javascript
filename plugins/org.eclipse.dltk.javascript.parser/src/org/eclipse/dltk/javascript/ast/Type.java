b07fc21f0386cf699059a5968becd1308e780b26
apanchenk <apanchenk>
2010-01-25 11:02:37 +0000

    type nodes

---
+++ plugins/org.eclipse.dltk.javascript.parser/src/org/eclipse/dltk/javascript/ast/Type.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2010 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.javascript.ast;
+
+import org.eclipse.dltk.ast.ASTNode;
+
+public abstract class Type extends Expression {
+
+	/**
+	 * @param parent
+	 */
+	public Type(ASTNode parent) {
+		super(parent);
+	}
+	
+	public abstract String getName();
+
+}
