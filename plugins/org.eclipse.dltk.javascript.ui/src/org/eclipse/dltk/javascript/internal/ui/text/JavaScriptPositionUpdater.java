7eaef73a55fae29415375c98299843c8d9cc1b81
apanchenk <apanchenk>
2010-01-27 10:19:35 +0000

    use new parser for XML highlighting

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/JavaScriptPositionUpdater.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/JavaScriptPositionUpdater.java
@@ -3,113 +3,70 @@
  */
 package org.eclipse.dltk.javascript.internal.ui.text;
 
-import java.io.CharArrayReader;
-import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
 
+import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
+import org.eclipse.dltk.ast.parser.ISourceParser;
+import org.eclipse.dltk.ast.parser.SourceParserManager;
+import org.eclipse.dltk.ast.utils.ASTUtil;
+import org.eclipse.dltk.compiler.CharOperation;
+import org.eclipse.dltk.javascript.ast.XmlFragment;
+import org.eclipse.dltk.javascript.ast.XmlLiteral;
+import org.eclipse.dltk.javascript.ast.XmlTextFragment;
+import org.eclipse.dltk.javascript.parser.JavaScriptParser;
 import org.eclipse.dltk.ui.editor.highlighting.AbstractSemanticHighlighter;
-
-import com.xored.org.mozilla.javascript.CompilerEnvirons;
-import com.xored.org.mozilla.javascript.ErrorReporter;
-import com.xored.org.mozilla.javascript.EvaluatorException;
-import com.xored.org.mozilla.javascript.IXMLCallback;
-import com.xored.org.mozilla.javascript.Parser;
 
 final class JavaScriptPositionUpdater extends AbstractSemanticHighlighter {
 
 	private static final int HL_XML_TAG = 0;
 	private static final int HL_XML_ATTRIBUTE = 1;
-	private static final int HL_XML_COMMENT = 1;
+	private static final int HL_XML_COMMENT = 2;
 
+	@Override
 	protected boolean doHighlighting(
 			org.eclipse.dltk.compiler.env.ISourceModule code) throws Exception {
-		char[] sourceAsCharArray = code.getContentsAsCharArray();
-
-		Parser p = new Parser(new CompilerEnvirons(), new ErrorReporter() {
-
-			public void error(String message, String sourceName, int line,
-					String lineSource, int offset) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public EvaluatorException runtimeError(String message,
-					String sourceName, int line, String lineSource,
-					int lineOffset) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public void warning(String message, String sourceName, int line,
-					String lineSource, int lineOffset) {
-				// TODO Auto-generated method stub
-
-			}
-
-		});
-
-		try {
-			p.setXMLCallback(new IXMLCallback() {
-
-				public void xmlTokenStart(int offset, String tagName, int cursor) {
-					int i = cursor - offset + 1;
-					if (tagName.length() != i) {
-						StringBuffer copy = new StringBuffer();
-						for (int a = 0; a < tagName.length(); a++) {
-							char c = tagName.charAt(a);
-							if (c == '\n')
-								copy.append("  "); //$NON-NLS-1$
-							else
-								copy.append(c);
-						}
-						tagName = copy.toString();
-					}
-
-					XMLTokenizer r = new XMLTokenizer(new StringReader(tagName));
-					List l = r.getRegions();
-					for (int a = 0; a < l.size(); a++) {
-						Token object = (Token) l.get(a);
-						if (object.context == XMLTokenizer.XML_TAG_NAME) {
-							addRange(offset - 1 + object.start,
-									object.textLength, HL_XML_TAG);
-						} else if (object.context == XMLTokenizer.XML_TAG_ATTRIBUTE_NAME) {
-							addRange(offset - 1 + object.start,
-									object.textLength, HL_XML_ATTRIBUTE);
-						} else if (object.context == XMLTokenizer.XML_COMMENT_OPEN
-								|| object.context == XMLTokenizer.XML_COMMENT_TEXT
-								|| object.context == XMLTokenizer.XML_COMMENT_CLOSE) {
-							addRange(offset - 1 + object.start,
-									object.textLength, HL_XML_COMMENT);
-						} else if (object.context == XMLTokenizer.XML_TAG_ATTRIBUTE_VALUE) {
-
-						} else {
-							// result.add(presenter
-							// .createHighlightedPosition(
-							// offset - 1
-							// + object.start,
-							// object.textLength,
-							// highlightings[3]));
-						}
-					}
-					// result.add(presenter.createHighlightedPosition(
-					// offset - 1, i,
-					// highlightings[0]));
-				}
-
-			});
-			p.parse(new CharArrayReader(sourceAsCharArray), this.toString(), 0);
-		} catch (IOException e) {
+		final ISourceParser parser = SourceParserManager.getInstance()
+				.getSourceParserById(JavaScriptParser.PARSER_ID);
+		if (parser == null) {
 			return false;
 		}
-		// result.add(presenter.createHighlightedPosition(offset , length,
-		// highlightings[0]));
-
+		final ModuleDeclaration declaration = parser.parse(
+				CharOperation.NO_CHAR, code.getContentsAsCharArray(), null);
+		if (declaration == null) {
+			return false;
+		}
+		for (XmlLiteral literal : ASTUtil.select(declaration, XmlLiteral.class)) {
+			for (XmlFragment fragment : literal.getFragments()) {
+				if (fragment instanceof XmlTextFragment) {
+					final XMLTokenizer tokenizer = new XMLTokenizer(
+							new StringReader(((XmlTextFragment) fragment)
+									.getXml()));
+					final int offset = fragment.sourceStart();
+					@SuppressWarnings("unchecked")
+					final List<Token> tokens = tokenizer.getRegions();
+					for (Token token : tokens) {
+						if (token.context == XMLTokenizer.XML_TAG_NAME) {
+							addRange(offset, token, HL_XML_TAG);
+						} else if (token.context == XMLTokenizer.XML_TAG_ATTRIBUTE_NAME) {
+							addRange(offset, token, HL_XML_ATTRIBUTE);
+						} else if (token.context == XMLTokenizer.XML_COMMENT_OPEN
+								|| token.context == XMLTokenizer.XML_COMMENT_TEXT
+								|| token.context == XMLTokenizer.XML_COMMENT_CLOSE) {
+							addRange(offset, token, HL_XML_COMMENT);
+						} else if (token.context == XMLTokenizer.XML_TAG_ATTRIBUTE_VALUE) {
+							//
+						}
+					}
+				}
+			}
+		}
 		return true;
 	}
 
-	private void addRange(int start, int len, int highlightingIndex) {
-		addPosition(start, start + len, highlightingIndex);
+	private void addRange(int start, Token token, int highlightingIndex) {
+		addPosition(start + token.start,
+				start + token.start + token.textLength, highlightingIndex);
 	}
 
 }
