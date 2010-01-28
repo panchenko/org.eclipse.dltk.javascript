e73d4227a8af13913b90323059cbc079cc40afd3
apanchenk <apanchenk>
2010-01-28 10:45:53 +0000

    java1.5 non-UI plugin

--- plugins/org.eclipse.dltk.javascript.core.dom.support/src/org/eclipse/dlkt/javascript/dom/support/internal/Activator.java
+++ plugins/org.eclipse.dltk.javascript.core.dom.support/src/org/eclipse/dlkt/javascript/dom/support/internal/Activator.java
@@ -1,38 +1,32 @@
 package org.eclipse.dlkt.javascript.dom.support.internal;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends Plugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.dlkt.javascript.dom.support";
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
@@ -40,7 +34,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
