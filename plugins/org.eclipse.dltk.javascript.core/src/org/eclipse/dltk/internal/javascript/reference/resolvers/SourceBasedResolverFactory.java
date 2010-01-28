4a66b5cdb38fc54c9cb33215a8eac830b9cbaf54
apanchenk <apanchenk>
2010-01-28 14:17:59 +0000

    no need provide empty implementation of IExecutableExtension

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/reference/resolvers/SourceBasedResolverFactory.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/reference/resolvers/SourceBasedResolverFactory.java
@@ -9,19 +9,10 @@
  *******************************************************************************/
 package org.eclipse.dltk.internal.javascript.reference.resolvers;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExecutableExtension;
-
-public class SourceBasedResolverFactory implements IResolverFactory,IExecutableExtension{
+public class SourceBasedResolverFactory implements IResolverFactory {
 
 	public IReferenceResolver create() {
 		return new SourceBasedResolver();
-	}
-
-	public void setInitializationData(IConfigurationElement config,
-			String propertyName, Object data) throws CoreException {
-		
 	}
 
 }
