32df6642b4d876531ba60c7f780760f0f0e5770f
apanchenk <apanchenk>
2010-01-28 14:18:03 +0000

    no need provide empty implementation of IExecutableExtension

--- plugins/org.eclipse.dltk.javascript.jdt.integration/src/org/eclipse/dltk/javascript/jdt/integration/JdtReferenceResolverFactory.java
+++ plugins/org.eclipse.dltk.javascript.jdt.integration/src/org/eclipse/dltk/javascript/jdt/integration/JdtReferenceResolverFactory.java
@@ -9,21 +9,13 @@
  *******************************************************************************/
 package org.eclipse.dltk.javascript.jdt.integration;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.dltk.internal.javascript.reference.resolvers.IReferenceResolver;
 import org.eclipse.dltk.internal.javascript.reference.resolvers.IResolverFactory;
 
-public class JdtReferenceResolverFactory implements IResolverFactory,IExecutableExtension{
+public class JdtReferenceResolverFactory implements IResolverFactory {
 
 	public IReferenceResolver create() {
 		return new JdtReferenceResolver();
-	}
-
-	public void setInitializationData(IConfigurationElement config,
-			String propertyName, Object data) throws CoreException {
-		
 	}
 
 }
