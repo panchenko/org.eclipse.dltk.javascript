39c5efa0c3ace90f2e5578559285c83c8019e026
apanchenk <apanchenk>
2010-01-28 17:29:14 +0000

    don't show tab policy controls here - they are available on the formatter page

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavascriptEditorPreferencesPage.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavascriptEditorPreferencesPage.java
@@ -9,7 +9,6 @@
  *******************************************************************************/
 package org.eclipse.dltk.javascript.internal.ui.preferences;
 
-
 import org.eclipse.dltk.javascript.internal.ui.JavaScriptUI;
 import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPreferencePage;
 import org.eclipse.dltk.ui.preferences.EditorConfigurationBlock;
@@ -18,22 +17,13 @@ import org.eclipse.dltk.ui.preferences.OverlayPreferenceStore;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
-
-
-
 public class JavascriptEditorPreferencesPage extends
 		AbstractConfigurationBlockPreferencePage {
 
-	/*
-	 * @see org.eclipse.ui.internal.editors.text.AbstractConfigureationBlockPreferencePage#getHelpId()
-	 */
 	protected String getHelpId() {
-		return "";
+		return ""; //$NON-NLS-1$
 	}
 
-	/*
-	 * @see org.eclipse.ui.internal.editors.text.AbstractConfigurationBlockPreferencePage#setDescription()
-	 */
 	protected void setDescription() {
 		String description = JavaScriptPreferenceMessages.JavascriptEditorPreferencePage_general;
 		setDescription(description);
@@ -43,18 +33,13 @@ public class JavascriptEditorPreferencesPage extends
 		return null;
 	}
 
-	/*
-	 * @see org.org.eclipse.ui.internal.editors.text.AbstractConfigurationBlockPreferencePage#setPreferenceStore()
-	 */
 	protected void setPreferenceStore() {
 		setPreferenceStore(JavaScriptUI.getDefault().getPreferenceStore());
 	}
 
-	/*
-	 * @see org.eclipse.ui.internal.editors.text.AbstractConfigureationBlockPreferencePage#createConfigurationBlock(org.eclipse.ui.internal.editors.text.OverlayPreferenceStore)
-	 */
 	protected IPreferenceConfigurationBlock createConfigurationBlock(
 			OverlayPreferenceStore overlayPreferenceStore) {
-		return new EditorConfigurationBlock(this, overlayPreferenceStore);
+		return new EditorConfigurationBlock(this, overlayPreferenceStore,
+				EditorConfigurationBlock.FLAG_EDITOR_SMART_NAVIGATION);
 	}
 }
