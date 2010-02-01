3e76a9388dca6fce47d8fa258e31cca82b3d075b
apanchenk <apanchenk>
2010-02-01 10:49:18 +0000

    extract xml completion

---
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/typeinference/TransparentRef.java
@@ -0,0 +1,212 @@
+/*******************************************************************************
+ * Copyright (c) 2010 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.internal.javascript.typeinference;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.dltk.internal.core.ModelElement;
+import org.eclipse.dltk.internal.javascript.reference.resolvers.ReferenceResolverContext;
+
+import com.xored.org.mozilla.javascript.Node;
+
+class TransparentRef implements IReference {
+
+	private final TypeInferencer typeInferencer;
+
+	IReference evaluateReference;
+
+	private final String fieldId;
+	private final Node node;
+
+	ReferenceResolverContext cs;
+
+	private boolean recursive = false;
+
+	TransparentRef(TypeInferencer typeInferencer, IReference evaluateReference,
+			Node objID, String fieldId, ModelElement parent,
+			ReferenceResolverContext cs) {
+		this.typeInferencer = typeInferencer;
+		this.evaluateReference = evaluateReference;
+		this.fieldId = fieldId;
+		this.node = objID;
+		this.parent = parent;
+		this.cs = cs;
+	}
+
+	public IReference getChild(String key, boolean resolveLocals) {
+		if (recursive)
+			return null;
+		try {
+			recursive = true;
+			IReference child = evaluateReference.getChild(key, resolveLocals);
+			return child;
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public Set<IReference> getChilds(boolean resolveLocals) {
+		if (recursive)
+			return Collections.emptySet();
+		try {
+			recursive = true;
+			return evaluateReference.getChilds(resolveLocals);
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public String getName() {
+		return fieldId;
+	}
+
+	public String getParentName() {
+		return fieldId;
+	}
+
+	public void setChild(String key, IReference ref) {
+		if (recursive)
+			return;
+		try {
+			recursive = true;
+			evaluateReference.setChild(key, ref);
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public boolean isChildishReference() {
+		return false;
+	}
+
+	ModelElement parent;
+
+	public void patchRef(HostCollection collection) {
+		if (recursive)
+			return;
+		try {
+			recursive = true;
+			Set s = evaluateReference.getChilds(false);
+			IReference queryElement = this.typeInferencer.internalEvaluate(
+					collection, getName(), node, parent, cs);
+
+			if (queryElement != null && queryElement != this) {
+				// make sure that this doesn't become a transparent to a
+				// transparent
+				// (because then circular references can happen)
+				// just point to the real reference.
+				while (queryElement instanceof TransparentRef) {
+					queryElement = ((TransparentRef) queryElement).evaluateReference;
+				}
+				if (!(queryElement instanceof CombinedOrReference && ((CombinedOrReference) queryElement)
+						.testContains(this))) {
+					this.evaluateReference = queryElement;
+				}
+			}
+			Iterator it = s.iterator();
+			// TODO REVIEW IT;
+			while (it.hasNext()) {
+				Object next = it.next();
+				if (!(next instanceof IReference))
+					continue;
+				IReference r = (IReference) next;
+				evaluateReference.setChild(r.getName(), r);
+			}
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public void recordDelete(String fieldId) {
+		if (recursive)
+			return;
+		try {
+			recursive = true;
+			evaluateReference.recordDelete(fieldId);
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public IReference getPrototype(boolean resolveLocals) {
+		if (recursive)
+			return null;
+		try {
+			recursive = true;
+			return evaluateReference.getPrototype(false);
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public void setPrototype(IReference ref) {
+		if (recursive)
+			return;
+		try {
+			recursive = true;
+			evaluateReference.setPrototype(ref);
+		} finally {
+			recursive = false;
+		}
+	}
+
+	int length;
+	int location;
+
+	public void addModelElements(Collection toAdd) {
+		if (parent != null)
+			toAdd.add(new FakeField(parent, getName(), location, length));
+	}
+
+	public void setLocationInformation(ModelElement mo, int position, int length) {
+		this.parent = mo;
+		this.location = position;
+		this.length = length;
+	}
+
+	public boolean isFunctionRef() {
+		if (recursive)
+			return false;
+		try {
+			recursive = true;
+			return evaluateReference.isFunctionRef();
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public boolean isLocal() {
+		if (recursive)
+			return false;
+		try {
+			recursive = true;
+			return evaluateReference.isLocal();
+		} finally {
+			recursive = false;
+		}
+	}
+
+	public void setLocal(boolean local) {
+		if (recursive)
+			return;
+		try {
+			recursive = true;
+			evaluateReference.setLocal(local);
+		} finally {
+			recursive = false;
+		}
+	}
+
+}
--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/typeinference/TypeInferencer.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/typeinference/TypeInferencer.java
@@ -9,8 +9,6 @@
  *******************************************************************************/
 package org.eclipse.dltk.internal.javascript.typeinference;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -24,17 +22,9 @@ import java.util.Set;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.eclipse.dltk.compiler.ISourceElementRequestor;
 import org.eclipse.dltk.internal.core.ModelElement;
 import org.eclipse.dltk.internal.javascript.reference.resolvers.ReferenceResolverContext;
-import org.eclipse.dltk.utils.TextUtils;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 import com.xored.org.mozilla.javascript.FunctionNode;
 import com.xored.org.mozilla.javascript.Node;
@@ -42,191 +32,6 @@ import com.xored.org.mozilla.javascript.ScriptOrFnNode;
 import com.xored.org.mozilla.javascript.Token;
 import com.xored.org.mozilla.javascript.Node.Jump;
 import com.xored.org.mozilla.javascript.Node.StringNode;
-
-final class TransparentRef implements IReference {
-	IReference evaluateReference;
-
-	private final String fieldId;
-	private final Node node;
-
-	ReferenceResolverContext cs;
-
-	private boolean recursive = false;
-
-	TransparentRef(IReference evaluateReference, Node objID, String fieldId,
-			ModelElement parent, ReferenceResolverContext cs) {
-		this.evaluateReference = evaluateReference;
-		this.fieldId = fieldId;
-		this.node = objID;
-		this.parent = parent;
-		this.cs = cs;
-	}
-
-	public IReference getChild(String key, boolean resolveLocals) {
-		if (recursive)
-			return null;
-		try {
-			recursive = true;
-			IReference child = evaluateReference.getChild(key, resolveLocals);
-			return child;
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public Set<IReference> getChilds(boolean resolveLocals) {
-		if (recursive)
-			return Collections.emptySet();
-		try {
-			recursive = true;
-			return evaluateReference.getChilds(resolveLocals);
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public String getName() {
-		return fieldId;
-	}
-
-	public String getParentName() {
-		return fieldId;
-	}
-
-	public void setChild(String key, IReference ref) {
-		if (recursive)
-			return;
-		try {
-			recursive = true;
-			evaluateReference.setChild(key, ref);
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public boolean isChildishReference() {
-		return false;
-	}
-
-	ModelElement parent;
-
-	public void patchRef(HostCollection collection) {
-		if (recursive)
-			return;
-		try {
-			recursive = true;
-			Set s = evaluateReference.getChilds(false);
-			IReference queryElement = TypeInferencer.internalEvaluate(
-					collection, getName(), node, parent, cs);
-
-			if (queryElement != null && queryElement != this) {
-				// make sure that this doesn't become a transparent to a
-				// transparent
-				// (because then circular references can happen)
-				// just point to the real reference.
-				while (queryElement instanceof TransparentRef) {
-					queryElement = ((TransparentRef) queryElement).evaluateReference;
-				}
-				if (!(queryElement instanceof CombinedOrReference && ((CombinedOrReference) queryElement)
-						.testContains(this))) {
-					this.evaluateReference = queryElement;
-				}
-			}
-			Iterator it = s.iterator();
-			// TODO REVIEW IT;
-			while (it.hasNext()) {
-				Object next = it.next();
-				if (!(next instanceof IReference))
-					continue;
-				IReference r = (IReference) next;
-				evaluateReference.setChild(r.getName(), r);
-			}
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public void recordDelete(String fieldId) {
-		if (recursive)
-			return;
-		try {
-			recursive = true;
-			evaluateReference.recordDelete(fieldId);
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public IReference getPrototype(boolean resolveLocals) {
-		if (recursive)
-			return null;
-		try {
-			recursive = true;
-			return evaluateReference.getPrototype(false);
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public void setPrototype(IReference ref) {
-		if (recursive)
-			return;
-		try {
-			recursive = true;
-			evaluateReference.setPrototype(ref);
-		} finally {
-			recursive = false;
-		}
-	}
-
-	int length;
-	int location;
-
-	public void addModelElements(Collection toAdd) {
-		if (parent != null)
-			toAdd.add(new FakeField(parent, getName(), location, length));
-	}
-
-	public void setLocationInformation(ModelElement mo, int position, int length) {
-		this.parent = mo;
-		this.location = position;
-		this.length = length;
-	}
-
-	public boolean isFunctionRef() {
-		if (recursive)
-			return false;
-		try {
-			recursive = true;
-			return evaluateReference.isFunctionRef();
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public boolean isLocal() {
-		if (recursive)
-			return false;
-		try {
-			recursive = true;
-			return evaluateReference.isLocal();
-		} finally {
-			recursive = false;
-		}
-	}
-
-	public void setLocal(boolean local) {
-		if (recursive)
-			return;
-		try {
-			recursive = true;
-			evaluateReference.setLocal(local);
-		} finally {
-			recursive = false;
-		}
-	}
-
-}
 
 public class TypeInferencer {
 
@@ -628,7 +433,8 @@ public class TypeInferencer {
 				pos = p1 + 1;
 			}
 			TransparentRef transparentRef = new TransparentRef(
-					evaluateReference, node.getLastChild(), fieldId, module, cs);
+					TypeInferencer.this, evaluateReference,
+					node.getLastChild(), fieldId, module, cs);
 			collection.addTransparent(transparentRef);
 			transparentRef.setLocationInformation(module, node.getPosition(),
 					fieldId.length());
@@ -730,7 +536,7 @@ public class TypeInferencer {
 		return internalEvaluate;
 	}
 
-	static IReference internalEvaluate(HostCollection collection, String key,
+	IReference internalEvaluate(HostCollection collection, String key,
 			Node expression, ModelElement parent, ReferenceResolverContext cs) {
 		if (expression == null)
 			return null;
@@ -783,7 +589,7 @@ public class TypeInferencer {
 				ref = resolveReferenceTree(key, cs, key1, ref);
 			}
 			if (ref != null) {
-				ref = new TransparentRef(ref, expression, key, parent, cs);
+				ref = new TransparentRef(this, ref, expression, key, parent, cs);
 			}
 			return ref;
 		default:
@@ -830,9 +636,8 @@ public class TypeInferencer {
 		return ws;
 	}
 
-	private static IReference createNewObjectRefernce(
-			HostCollection collection2, String key, Node expression,
-			ReferenceResolverContext cs) {
+	private IReference createNewObjectRefernce(HostCollection collection2,
+			String key, Node expression, ReferenceResolverContext cs) {
 		Node nm = expression.getFirstChild();
 		String id = getObjId(nm);
 		if (id == null)
@@ -842,15 +647,19 @@ public class TypeInferencer {
 				key, false);
 		if (id.equals("XML")) {
 			try {
-				String string = expression.getLastChild().getString();
-				modifyReferenceXML(unknownReference, string, cs);
+				if (xmlInferencer == null) {
+					xmlInferencer = new XMLLiteralInferencer();
+				}
+				xmlInferencer.modifyReference(unknownReference, expression
+						.getLastChild().getString(), cs);
 			} catch (ClassCastException e) {
 			}
 		}
 		CombinedOrReference ws = new CombinedOrReference(ref, unknownReference);
 		return ws;
-
 	}
+
+	private XMLLiteralInferencer xmlInferencer = null;
 
 	private static IReference createArrayLiteralReference(
 			HostCollection collection2, String key, Node expression,
@@ -865,94 +674,8 @@ public class TypeInferencer {
 
 	}
 
-	static SAXParser parser;
-
-	static {
-		try {
-			parser = SAXParserFactory.newInstance().newSAXParser();
-		} catch (ParserConfigurationException e) {
-			throw new LinkageError();
-		} catch (SAXException e) {
-			throw new LinkageError();
-		}
-	}
-
-	private static void modifyReferenceXML(
-			final StandardSelfCompletingReference uncknownReference,
-			String string, final ReferenceResolverContext cs) {
-		try {
-			parser.parse(new ByteArrayInputStream(string.getBytes()),
-					new DefaultHandler() {
-
-						boolean has = false;
-						IReference curReference = uncknownReference;
-						Stack stack = new Stack();
-
-						public void endElement(String uri, String localName,
-								String name) throws SAXException {
-							if (!stack.isEmpty()) {
-								stack.pop();
-								if (!stack.isEmpty()) {
-									curReference = (IReference) stack.peek();
-								} else {
-									curReference = uncknownReference;
-								}
-							} else {
-								curReference = uncknownReference;
-							}
-						}
-
-						public void startElement(String uri, String localName,
-								String name, Attributes attributes)
-								throws SAXException {
-							if (name.indexOf(':') != -1) {
-								name = TextUtils.replace(name, ':', "::");
-							}
-
-							NewReference elementNewRef = new NewReference(name,
-									"XML", cs);
-							StandardSelfCompletingReference uncknownReference2 = new StandardSelfCompletingReference(
-									name, true);
-
-							CombinedOrReference corf = new CombinedOrReference(
-									elementNewRef, uncknownReference2);
-
-							int length = attributes.getLength();
-							if (has) {
-								curReference.setChild(name, corf);
-								curReference = corf;
-								stack.push(curReference);
-							}
-							for (int a = 0; a < length; a++) {
-								String val = "@" + attributes.getQName(a);
-
-								if (val.indexOf(':') != -1) {
-									val = TextUtils.replace(val, ':', "::");
-								}
-
-								NewReference attrNewRef = new NewReference(val,
-										"XMLATTR", null);
-								StandardSelfCompletingReference uncknownReference3 = new StandardSelfCompletingReference(
-										val, true);
-								CombinedOrReference attrCorf = new CombinedOrReference(
-										attrNewRef, uncknownReference3);
-
-								curReference.setChild(val, attrCorf);
-							}
-							has = true;
-						}
-
-					});
-		} catch (SAXException e) {
-
-		} catch (IOException e) {
-
-		}
-	}
-
-	private static IReference createObjectLiteral(HostCollection col,
-			String key, Node expression, ModelElement parent,
-			ReferenceResolverContext cs) {
+	private IReference createObjectLiteral(HostCollection col, String key,
+			Node expression, ModelElement parent, ReferenceResolverContext cs) {
 		Object[] ids = (Object[]) expression.getProp(Node.OBJECT_IDS_PROP);
 		ArrayList positions = (ArrayList) expression
 				.getProp(Node.DESCENDANTS_FLAG);
