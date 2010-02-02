219dfd1d4f954b114e97ea5661acc21f6a37f894
apanchenk <apanchenk>
2010-02-02 09:43:55 +0000

    generics, change char[][] to String[] for parameters

--- plugins/org.eclipse.dltk.javascript.jdt.integration/src/org/eclipse/dltk/javascript/jdt/integration/JavaProposalReference.java
+++ plugins/org.eclipse.dltk.javascript.jdt.integration/src/org/eclipse/dltk/javascript/jdt/integration/JavaProposalReference.java
@@ -14,6 +14,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.dltk.compiler.CharOperation;
 import org.eclipse.dltk.core.IModelElement;
 import org.eclipse.dltk.internal.core.ModelElement;
 import org.eclipse.dltk.internal.javascript.reference.resolvers.ReferenceResolverContext;
@@ -233,16 +234,18 @@ public class JavaProposalReference extends StandardSelfCompletingReference
 		return kind;
 	}
 
-	public char[] getDeclarationSignature() {
-		return proposal.getDeclarationSignature();
-	}
+	// public char[] getDeclarationSignature() {
+	// return proposal.getDeclarationSignature();
+	// }
 
-	public char[] getSignature() {
-		return proposal.getSignature();
-	}
+	// public char[] getSignature() {
+	// return proposal.getSignature();
+	// }
 
-	public char[][] getParameterNames() {
-		return proposal.findParameterNames(null);
+	@Override
+	public String[] getParameterNames() {
+		return CharOperation.charArrayToStringArray(proposal
+				.findParameterNames(null));
 	}
 
 }
