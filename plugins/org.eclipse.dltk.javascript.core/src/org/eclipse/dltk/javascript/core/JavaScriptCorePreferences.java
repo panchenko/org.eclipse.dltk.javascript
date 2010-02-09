ee5a2dee6264ca18039682177ff8b503dee7e16b
apanchenk <apanchenk>
2010-02-09 14:02:09 +0000

    isStrictMode() method extracted

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/core/JavaScriptCorePreferences.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/core/JavaScriptCorePreferences.java
@@ -3,11 +3,16 @@
  */
 package org.eclipse.dltk.javascript.core;
 
+import org.eclipse.core.runtime.Platform;
+
 /**
  * @author jcompagner
- * 
  */
 public class JavaScriptCorePreferences {
 	public final static String USE_STRICT_MODE = "USE_STRICT_MODE";
 
+	public static boolean isStrictMode() {
+		return Platform.getPreferencesService().getBoolean(
+				JavaScriptPlugin.PLUGIN_ID, USE_STRICT_MODE, false, null);
+	}
 }
