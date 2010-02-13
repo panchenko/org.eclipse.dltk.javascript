63afb05a539275b7d1b0ae904613032da1ff3271
apanchenk <apanchenk>
2010-02-13 06:04:57 +0000

    new line insertion tests

--- tests/org.eclipse.dltk.javascript.formatter.tests/scripts/statements.js
+++ tests/org.eclipse.dltk.javascript.formatter.tests/scripts/statements.js
@@ -1,11 +1,26 @@
-==== assignments-new-line
+==== assignments-new-lines
 a=1;b=2;c=3;
 ==
 a = 1;
 b = 2;
 c = 3;
+==== switch-new-lines
+function hello(){switch(a){case 0:return 0;default:return 1;}}
+==
+function hello () {
+	switch (a) {
+		case 0:
+			return 0;
+		default:
+			return 1;
+	}
+}
 ==> formatter.statement.new.line = false
 ==== assignments-no-new-line
 a=1;b=2;c=3;
 ==
 a = 1;b = 2;c = 3;
+==== switch-no-new-lines
+function hello(){switch(a){case 0:return 0;default:return 1;}}
+==
+function hello () {switch (a) {case 0:return 0;default:return 1;}}
