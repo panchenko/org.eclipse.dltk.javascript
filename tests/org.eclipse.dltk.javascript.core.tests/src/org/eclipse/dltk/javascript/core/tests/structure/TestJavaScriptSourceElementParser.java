2b7daeefaeef1a139d3109c8d5f1ba03f8121893
apanchenk <apanchenk>
2010-02-27 10:33:46 +0000

    extract TestTypeInferencer2 class

--- tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/structure/TestJavaScriptSourceElementParser.java
+++ tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/structure/TestJavaScriptSourceElementParser.java
@@ -3,20 +3,13 @@ package org.eclipse.dltk.javascript.core.tests.structure;
 import org.eclipse.dltk.compiler.env.IModuleSource;
 import org.eclipse.dltk.internal.javascript.parser.JavaScriptSourceElementParser2;
 import org.eclipse.dltk.internal.javascript.ti.TypeInferencer2;
+import org.eclipse.dltk.javascript.core.tests.typeinference.TestTypeInferencer2;
 
-/**
- * 
- */
 final class TestJavaScriptSourceElementParser extends
 		JavaScriptSourceElementParser2 {
 	@Override
 	protected TypeInferencer2 createInferencer() {
-		return new TypeInferencer2() {
-			@Override
-			protected void log(Throwable e) {
-				NewStructureParserTests.fail(e.toString());
-			}
-		};
+		return new TestTypeInferencer2();
 	}
 
 	@Override
---
+++ tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/typeinference/TestTypeInferencer2.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2010 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.javascript.core.tests.typeinference;
+
+import junit.framework.Assert;
+
+import org.eclipse.dltk.internal.javascript.ti.TypeInferencer2;
+
+public class TestTypeInferencer2 extends TypeInferencer2 {
+	@Override
+	protected void log(Throwable e) {
+		Assert.fail(e.toString());
+	}
+}
