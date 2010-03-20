44dbad4f9797b3e90c2ebead5833d2d014983593
apanchenk <apanchenk>
2010-03-20 16:49:08 +0000

    correct getter & setter names

--- plugins/org.eclipse.dltk.javascript.formatter/src/org/eclipse/dltk/javascript/formatter/internal/FormatterNodeBuilder.java
+++ plugins/org.eclipse.dltk.javascript.formatter/src/org/eclipse/dltk/javascript/formatter/internal/FormatterNodeBuilder.java
@@ -1638,7 +1638,7 @@ public class FormatterNodeBuilder extends AbstractFormatterNodeBuilder {
 						document);
 
 				formatterNode.setBegin(createTextNode(document, node
-						.getVoidKeyword()));
+						.getYieldKeyword()));
 
 				push(formatterNode);
 
