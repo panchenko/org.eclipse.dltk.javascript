69b8ae26950f9338aecc5a9633dd7e99ce56d2a5
apanchenk <apanchenk>
2010-04-16 08:41:36 +0000

    enable typeinfo

--- tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/structure/TestJavaScriptSourceElementParser.java
+++ tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/structure/TestJavaScriptSourceElementParser.java
@@ -11,8 +11,10 @@
  *******************************************************************************/
 package org.eclipse.dltk.javascript.core.tests.structure;
 
+import org.eclipse.dltk.compiler.env.IModuleSource;
 import org.eclipse.dltk.internal.javascript.parser.JavaScriptSourceElementParser2;
 import org.eclipse.dltk.internal.javascript.ti.TypeInferencer2;
+import org.eclipse.dltk.javascript.ast.Script;
 import org.eclipse.dltk.javascript.core.tests.typeinference.TestTypeInferencer2;
 import org.eclipse.dltk.javascript.parser.JavaScriptParser;
 
@@ -24,10 +26,10 @@ final class TestJavaScriptSourceElementParser extends
 	}
 
 	@Override
-	protected JavaScriptParser createParser() {
-		final JavaScriptParser parser = super.createParser();
+	protected Script parse(IModuleSource module) {
+		final JavaScriptParser parser = new JavaScriptParser();
 		parser.setTypeInformationEnabled(true);
-		return parser;
+		return parser.parse(module, fReporter);
 	}
 
 }
