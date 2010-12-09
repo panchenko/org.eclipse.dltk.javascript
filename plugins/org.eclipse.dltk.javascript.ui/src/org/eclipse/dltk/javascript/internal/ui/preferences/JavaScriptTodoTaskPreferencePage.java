5219d78ceb702130b1c0404d3880caeb7ae78eab
apanchenk <apanchenk>
2010-12-09 11:28:42 +0000

    empty body of setPreferenceStore() - we don't use it

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavaScriptTodoTaskPreferencePage.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavaScriptTodoTaskPreferencePage.java
@@ -3,7 +3,6 @@ package org.eclipse.dltk.javascript.internal.ui.preferences;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.dltk.javascript.core.JavaScriptNature;
 import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
-import org.eclipse.dltk.ui.PreferencesAdapter;
 import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage;
 import org.eclipse.dltk.ui.preferences.AbstractOptionsBlock;
 import org.eclipse.dltk.ui.preferences.TodoTaskOptionsBlock;
@@ -37,8 +36,7 @@ public class JavaScriptTodoTaskPreferencePage extends
 	}
 
 	protected void setPreferenceStore() {
-		setPreferenceStore(new PreferencesAdapter(JavaScriptPlugin.getDefault()
-				.getPluginPreferences()));
+		// empty
 	}
 
 	protected String getPreferencePageId() {
