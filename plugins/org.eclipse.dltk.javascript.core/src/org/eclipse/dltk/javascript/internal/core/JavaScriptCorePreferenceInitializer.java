a01a7068dcdeb43e7bdddf144547c74ef02aef29
jcompagne <jcompagne>
2010-12-28 15:31:43 +0000

    removed STRICT mode preference/boolean is not used anymore , all errors are configurable by itself

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/internal/core/JavaScriptCorePreferenceInitializer.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/internal/core/JavaScriptCorePreferenceInitializer.java
@@ -4,7 +4,6 @@ import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.dltk.compiler.task.TaskTagUtils;
-import org.eclipse.dltk.javascript.core.JavaScriptCorePreferences;
 import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
 
 public class JavaScriptCorePreferenceInitializer extends
@@ -14,8 +13,6 @@ public class JavaScriptCorePreferenceInitializer extends
 	public void initializeDefaultPreferences() {
 		final IEclipsePreferences preferences = new DefaultScope()
 				.getNode(JavaScriptPlugin.PLUGIN_ID);
-		preferences
-				.putBoolean(JavaScriptCorePreferences.USE_STRICT_MODE, false);
 		TaskTagUtils.initializeDefaultValues(preferences);
 	}
 }
