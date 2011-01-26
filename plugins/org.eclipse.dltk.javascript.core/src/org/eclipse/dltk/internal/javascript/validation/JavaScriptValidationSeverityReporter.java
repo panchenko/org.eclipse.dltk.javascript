af80103db7b8de53d1bbad71b89958b3e0126b65
apanchenk <apanchenk>
2011-01-26 10:33:17 +0000

    ProblemSeverity enum

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/validation/JavaScriptValidationSeverityReporter.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/validation/JavaScriptValidationSeverityReporter.java
@@ -3,28 +3,29 @@ package org.eclipse.dltk.internal.javascript.validation;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.dltk.compiler.problem.DefaultProblemIdentifier;
 import org.eclipse.dltk.compiler.problem.IProblemIdentifier;
-import org.eclipse.dltk.compiler.problem.ProblemSeverities;
+import org.eclipse.dltk.compiler.problem.ProblemSeverity;
 import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
 import org.eclipse.dltk.javascript.parser.ISeverityReporter;
 import org.eclipse.dltk.javascript.parser.Reporter.Severity;
 
 public class JavaScriptValidationSeverityReporter implements ISeverityReporter {
 
-	public Severity getSeverity(IProblemIdentifier problemId,
-			Severity defaultSeverity) {
+	public ProblemSeverity getSeverity(IProblemIdentifier problemId,
+			ProblemSeverity defaultSeverity) {
 		if (defaultSeverity == null)
 			defaultSeverity = Severity.WARNING;
-		int defaultProblemSeverity = defaultSeverity == Severity.WARNING ? ProblemSeverities.Warning
-				: ProblemSeverities.Error;
 
-		int severity = new InstanceScope().getNode(JavaScriptPlugin.PLUGIN_ID)
-				.getInt(DefaultProblemIdentifier.encode(problemId),
-						defaultProblemSeverity);
-		switch (severity) {
-		case ProblemSeverities.Error:
-			return Severity.ERROR;
-		case ProblemSeverities.Ignore:
-			return null;
+		String severity = new InstanceScope().getNode(
+				JavaScriptPlugin.PLUGIN_ID).get(
+				DefaultProblemIdentifier.encode(problemId), null);
+		if (severity != null) {
+			if (ProblemSeverity.ERROR.name().equals(severity)) {
+				return ProblemSeverity.ERROR;
+			} else if (ProblemSeverity.WARNING.name().equals(severity)) {
+				return ProblemSeverity.WARNING;
+			} else if (ProblemSeverity.IGNORE.name().equals(severity)) {
+				return null;
+			}
 		}
 		return defaultSeverity;
 	}
