57038823f8b47ad9b93201713bc9fa7410a8e734
jcompagne <jcompagne>
2011-02-17 10:18:47 +0000

    also add the info as a severity to all the things that are checked

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/validation/JavaScriptValidationSeverityReporter.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/validation/JavaScriptValidationSeverityReporter.java
@@ -22,6 +22,8 @@ public class JavaScriptValidationSeverityReporter implements ISeverityReporter {
 				return ProblemSeverity.ERROR;
 			} else if (ProblemSeverity.WARNING.name().equals(severity)) {
 				return ProblemSeverity.WARNING;
+			} else if (ProblemSeverity.INFO.name().equals(severity)) {
+				return ProblemSeverity.INFO;
 			} else if (ProblemSeverity.IGNORE.name().equals(severity)) {
 				return null;
 			}
