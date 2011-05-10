a4de5e1e98ed0c494ecb963ecfe4f1e699e8805d
apanchenk <apanchenk>
2011-05-10 15:58:47 +0000

    lazy type evaluation coming from assignment

--- tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/validation/TypeInfoValidationTests.java
+++ tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/validation/TypeInfoValidationTests.java
@@ -506,6 +506,20 @@ public class TypeInfoValidationTests extends AbstractValidationTest {
 		assertEquals(problems.toString(), 0, problems.size());
 	}
 
+	public void testLazyInnerReference1() {
+		StringList code = new StringList();
+		code.add("var NUM");
+		code.add("NUM = function() {");
+		code.add("  /** @return {Number} */");
+		code.add("  var q = function() {");
+		code.add("    return new NUM();");
+		code.add("  }");
+		code.add("  return Number;");
+		code.add("}()");
+		final List<IProblem> problems = validate(code.toString());
+		assertEquals(problems.toString(), 0, problems.size());
+	}
+
 	public void testGenericParamWtihNoneGenericCall() {
 		List<String> code = new StringList();
 		code.add("/**");
