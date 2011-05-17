1731108786df39a7613bba300e3798234134aeb5
apanchenk <apanchenk>
2011-05-17 14:59:19 +0000

    enum Visibility

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/typeinfo/model/impl/MemberImpl.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/typeinfo/model/impl/MemberImpl.java
@@ -9,7 +9,7 @@
  * Contributors:
  *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
  *
- * $Id: MemberImpl.java,v 1.9 2011/02/10 13:05:30 apanchenk Exp $
+ * $Id: MemberImpl.java,v 1.10 2011/05/17 14:59:19 apanchenk Exp $
  */
 package org.eclipse.dltk.javascript.typeinfo.model.impl;
 
@@ -20,6 +20,7 @@ import org.eclipse.dltk.javascript.typeinfo.model.Type;
 import org.eclipse.dltk.javascript.typeinfo.model.TypeInfoModelPackage;
 import org.eclipse.dltk.javascript.typeinfo.model.TypeRef;
 import org.eclipse.dltk.javascript.typeinfo.model.TypedElement;
+import org.eclipse.dltk.javascript.typeinfo.model.Visibility;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EClass;
@@ -37,6 +38,7 @@ import org.eclipse.emf.ecore.impl.ENotificationImpl;
  *   <li>{@link org.eclipse.dltk.javascript.typeinfo.model.impl.MemberImpl#getDirectType <em>Direct Type</em>}</li>
  *   <li>{@link org.eclipse.dltk.javascript.typeinfo.model.impl.MemberImpl#isStatic <em>Static</em>}</li>
  *   <li>{@link org.eclipse.dltk.javascript.typeinfo.model.impl.MemberImpl#getDeclaringType <em>Declaring Type</em>}</li>
+ *   <li>{@link org.eclipse.dltk.javascript.typeinfo.model.impl.MemberImpl#getVisibility <em>Visibility</em>}</li>
  * </ul>
  * </p>
  *
@@ -74,6 +76,26 @@ public abstract class MemberImpl extends ElementImpl implements Member {
 	protected boolean static_ = STATIC_EDEFAULT;
 
 	/**
+     * The default value of the '{@link #getVisibility() <em>Visibility</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getVisibility()
+     * @generated
+     * @ordered
+     */
+    protected static final Visibility VISIBILITY_EDEFAULT = Visibility.PUBLIC;
+
+    /**
+     * The cached value of the '{@link #getVisibility() <em>Visibility</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getVisibility()
+     * @generated
+     * @ordered
+     */
+    protected Visibility visibility = VISIBILITY_EDEFAULT;
+
+    /**
      * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
      * @generated
@@ -186,6 +208,27 @@ public abstract class MemberImpl extends ElementImpl implements Member {
 
 	/**
      * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public Visibility getVisibility() {
+        return visibility;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setVisibility(Visibility newVisibility) {
+        Visibility oldVisibility = visibility;
+        visibility = newVisibility == null ? VISIBILITY_EDEFAULT : newVisibility;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, TypeInfoModelPackage.MEMBER__VISIBILITY, oldVisibility, visibility));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
      * @generated
      */
@@ -246,6 +289,8 @@ public abstract class MemberImpl extends ElementImpl implements Member {
                 return isStatic();
             case TypeInfoModelPackage.MEMBER__DECLARING_TYPE:
                 return getDeclaringType();
+            case TypeInfoModelPackage.MEMBER__VISIBILITY:
+                return getVisibility();
         }
         return super.eGet(featureID, resolve, coreType);
     }
@@ -266,6 +311,9 @@ public abstract class MemberImpl extends ElementImpl implements Member {
                 return;
             case TypeInfoModelPackage.MEMBER__STATIC:
                 setStatic((Boolean)newValue);
+                return;
+            case TypeInfoModelPackage.MEMBER__VISIBILITY:
+                setVisibility((Visibility)newValue);
                 return;
         }
         super.eSet(featureID, newValue);
@@ -288,6 +336,9 @@ public abstract class MemberImpl extends ElementImpl implements Member {
             case TypeInfoModelPackage.MEMBER__STATIC:
                 setStatic(STATIC_EDEFAULT);
                 return;
+            case TypeInfoModelPackage.MEMBER__VISIBILITY:
+                setVisibility(VISIBILITY_EDEFAULT);
+                return;
         }
         super.eUnset(featureID);
     }
@@ -308,6 +359,8 @@ public abstract class MemberImpl extends ElementImpl implements Member {
                 return static_ != STATIC_EDEFAULT;
             case TypeInfoModelPackage.MEMBER__DECLARING_TYPE:
                 return getDeclaringType() != null;
+            case TypeInfoModelPackage.MEMBER__VISIBILITY:
+                return visibility != VISIBILITY_EDEFAULT;
         }
         return super.eIsSet(featureID);
     }
@@ -358,6 +411,8 @@ public abstract class MemberImpl extends ElementImpl implements Member {
         StringBuffer result = new StringBuffer(super.toString());
         result.append(" (static: "); //$NON-NLS-1$
         result.append(static_);
+        result.append(", visibility: "); //$NON-NLS-1$
+        result.append(visibility);
         result.append(')');
         return result.toString();
     }
