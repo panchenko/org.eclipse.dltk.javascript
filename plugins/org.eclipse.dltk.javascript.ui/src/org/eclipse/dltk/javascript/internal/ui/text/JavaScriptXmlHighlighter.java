89fd5ddd23eacb1aeb2ecb29fb305d16bc2a7a54
apanchenk <apanchenk>
2012-02-07 12:50:33 +0000

    Highlight using ASTVisitor, so we detect nested elements for object literals.

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/JavaScriptXmlHighlighter.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/JavaScriptXmlHighlighter.java
@@ -3,7 +3,8 @@ package org.eclipse.dltk.javascript.internal.ui.text;
 import java.io.StringReader;
 import java.util.List;
 
-import org.eclipse.dltk.ast.utils.ASTUtil;
+import org.eclipse.dltk.ast.ASTNode;
+import org.eclipse.dltk.ast.ASTVisitor;
 import org.eclipse.dltk.compiler.env.IModuleSource;
 import org.eclipse.dltk.javascript.ast.Expression;
 import org.eclipse.dltk.javascript.ast.ForEachInStatement;
@@ -11,7 +12,6 @@ import org.eclipse.dltk.javascript.ast.GetMethod;
 import org.eclipse.dltk.javascript.ast.Identifier;
 import org.eclipse.dltk.javascript.ast.Keyword;
 import org.eclipse.dltk.javascript.ast.Method;
-import org.eclipse.dltk.javascript.ast.ObjectInitializer;
 import org.eclipse.dltk.javascript.ast.PropertyInitializer;
 import org.eclipse.dltk.javascript.ast.RegExpLiteral;
 import org.eclipse.dltk.javascript.ast.Script;
@@ -20,6 +20,7 @@ import org.eclipse.dltk.javascript.ast.StringLiteral;
 import org.eclipse.dltk.javascript.ast.XmlFragment;
 import org.eclipse.dltk.javascript.ast.XmlLiteral;
 import org.eclipse.dltk.javascript.ast.XmlTextFragment;
+import org.eclipse.dltk.javascript.internal.ui.JavaScriptUI;
 import org.eclipse.dltk.javascript.parser.JavaScriptParserUtil;
 import org.eclipse.dltk.ui.editor.highlighting.AbortSemanticHighlightingException;
 import org.eclipse.dltk.ui.editor.highlighting.ISemanticHighlighter;
@@ -46,7 +47,40 @@ public class JavaScriptXmlHighlighter extends AbstractJavaScriptHighlighter
 		if (declaration == null) {
 			throw new AbortSemanticHighlightingException();
 		}
-		for (XmlLiteral literal : ASTUtil.select(declaration, XmlLiteral.class)) {
+		try {
+			declaration.traverse(new HighlightingVisitor(requestor));
+		} catch (Exception e) {
+			JavaScriptUI.log(e);
+		}
+
+	}
+
+	static class HighlightingVisitor extends ASTVisitor {
+		final ISemanticHighlightingRequestor requestor;
+		final boolean bPropertyNames = isSemanticHighlightingEnabled(HL_PROPERTY);
+
+		public HighlightingVisitor(ISemanticHighlightingRequestor requestor) {
+			this.requestor = requestor;
+		}
+
+		public boolean visitGeneral(ASTNode node) throws Exception {
+			if (node instanceof XmlLiteral) {
+				handleXML((XmlLiteral) node);
+			} else if (node instanceof Method) {
+				handleMethod((Method) node);
+			} else if (node instanceof RegExpLiteral) {
+				handleRegExp((RegExpLiteral) node);
+			} else if (node instanceof PropertyInitializer) {
+				if (bPropertyNames) {
+					handlePropertyInitializer((PropertyInitializer) node);
+				}
+			} else if (node instanceof ForEachInStatement) {
+				handleForEachIn((ForEachInStatement) node);
+			}
+			return true;
+		}
+
+		private void handleXML(XmlLiteral literal) {
 			for (XmlFragment fragment : literal.getFragments()) {
 				if (fragment instanceof XmlTextFragment) {
 					final XMLTokenizer tokenizer = new XMLTokenizer(
@@ -71,49 +105,44 @@ public class JavaScriptXmlHighlighter extends AbstractJavaScriptHighlighter
 				}
 			}
 		}
-		for (Method method : ASTUtil.select(declaration, Method.class)) {
+
+		private void handleMethod(Method method) {
 			Keyword keyword;
 			if (method instanceof GetMethod) {
 				keyword = ((GetMethod) method).getGetKeyword();
 			} else if (method instanceof SetMethod) {
 				keyword = ((SetMethod) method).getSetKeyword();
 			} else {
-				continue;
+				return;
 			}
 			requestor.addPosition(keyword.sourceStart(), keyword.sourceEnd(),
 					HL_KEYWORD);
 		}
-		for (ForEachInStatement forEach : ASTUtil.select(declaration,
-				ForEachInStatement.class)) {
+
+		private void handleForEachIn(ForEachInStatement forEach) {
 			final Keyword keyword = forEach.getEachKeyword();
 			requestor.addPosition(keyword.sourceStart(), keyword.sourceEnd(),
 					HL_KEYWORD);
 		}
-		for (RegExpLiteral regExp : ASTUtil.select(declaration,
-				RegExpLiteral.class)) {
+
+		private void handleRegExp(RegExpLiteral regExp) {
 			requestor.addPosition(regExp.sourceStart(), regExp.sourceEnd(),
 					HL_REGEXP);
 		}
-		if (isSemanticHighlightingEnabled(HL_PROPERTY)) {
-			for (ObjectInitializer object : ASTUtil.select(declaration,
-					ObjectInitializer.class)) {
-				for (PropertyInitializer property : object
-						.getPropertyInitializers()) {
-					final Expression name = property.getName();
-					if (name instanceof Identifier
-							|| name instanceof StringLiteral) {
-						requestor.addPosition(name.sourceStart(),
-								name.sourceEnd(), HL_PROPERTY);
-					}
-				}
+
+		private void handlePropertyInitializer(PropertyInitializer property) {
+			final Expression name = property.getName();
+			if (name instanceof Identifier || name instanceof StringLiteral) {
+				requestor.addPosition(name.sourceStart(), name.sourceEnd(),
+						HL_PROPERTY);
 			}
 		}
-	}
 
-	private static void addRange(ISemanticHighlightingRequestor requestor,
-			int start, Token token, String highlightingKey) {
-		requestor.addPosition(start + token.start, start + token.start
-				+ token.textLength, highlightingKey);
+		private static void addRange(ISemanticHighlightingRequestor requestor,
+				int start, Token token, String highlightingKey) {
+			requestor.addPosition(start + token.start, start + token.start
+					+ token.textLength, highlightingKey);
+		}
 	}
 
 }
