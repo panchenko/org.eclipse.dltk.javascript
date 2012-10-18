3f489590f105545ee7aa7c88b06bd8862a5f43d3
Alex Panchenko <alex.panchenko@gmail.com>
2012-10-18 11:11:46 +0700

    move debug code to tests

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/structure/JavaScriptSourceElementParser3.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/internal/javascript/parser/structure/JavaScriptSourceElementParser3.java
@@ -18,7 +18,6 @@ import org.eclipse.dltk.core.ISourceElementParser;
 import org.eclipse.dltk.javascript.ast.Script;
 import org.eclipse.dltk.javascript.parser.JavaScriptParserUtil;
 import org.eclipse.dltk.javascript.structure.IStructureNode;
-import org.eclipse.dltk.javascript.structure.ScriptScope;
 import org.eclipse.dltk.javascript.typeinfo.ReferenceSource;
 
 public class JavaScriptSourceElementParser3 implements ISourceElementParser {
@@ -38,38 +37,20 @@ public class JavaScriptSourceElementParser3 implements ISourceElementParser {
 		final Script script = parse(module);
 		final StructureReporter3 reporter = new StructureReporter3(
 				new ReferenceSource(module.getModelElement()));
-		fRequestor.enterModule();
 		final IStructureNode node = reporter.visit(script);
+		reportStructure(script, node);
+	}
+
+	protected void reportStructure(final Script script,
+			final IStructureNode node) {
+		fRequestor.enterModule();
 		node.reportStructure(new StructureRequestor(fRequestor),
 				new StructureContext());
 		fRequestor.exitModule(script.sourceEnd());
-		if (DEBUG) {
-			// TODO (alex) remove before merging to master.
-			print(node, true, 0);
-		}
-	}
-
-	private void print(IStructureNode node, boolean printable, int level) {
-		if (node.isManyChildren()) {
-			final int nextLevel = node instanceof ScriptScope ? level
-					: level + 1;
-			for (IStructureNode child : node.getChildren()) {
-				for (int i = 0; i < nextLevel; ++i) {
-					System.out.print('\t');
-				}
-				System.out.println(child);
-				print(child, true, nextLevel);
-			}
-		} else {
-			for (IStructureNode child : node.getChildren()) {
-				print(child, false, level);
-			}
-		}
 	}
 
 	protected Script parse(IModuleSource module) {
 		return JavaScriptParserUtil.parse(module, fReporter);
 	}
 
-	private static final boolean DEBUG = true;
 }
--- tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/structure/TestJavaScriptSourceElementParser.java
+++ tests/org.eclipse.dltk.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/structure/TestJavaScriptSourceElementParser.java
@@ -15,13 +15,40 @@ import org.eclipse.dltk.compiler.env.IModuleSource;
 import org.eclipse.dltk.internal.javascript.parser.structure.JavaScriptSourceElementParser3;
 import org.eclipse.dltk.javascript.ast.Script;
 import org.eclipse.dltk.javascript.parser.JavaScriptParser;
+import org.eclipse.dltk.javascript.structure.IStructureNode;
+import org.eclipse.dltk.javascript.structure.ScriptScope;
 
+@SuppressWarnings("restriction")
 public final class TestJavaScriptSourceElementParser extends
 		JavaScriptSourceElementParser3 {
 	@Override
 	protected Script parse(IModuleSource module) {
 		final JavaScriptParser parser = new JavaScriptParser();
 		return parser.parse(module, fReporter);
+	}
+
+	@Override
+	protected void reportStructure(Script script, IStructureNode node) {
+		print(node, true, 0);
+		super.reportStructure(script, node);
+	}
+
+	private void print(IStructureNode node, boolean printable, int level) {
+		if (node.isManyChildren()) {
+			final int nextLevel = node instanceof ScriptScope ? level
+					: level + 1;
+			for (IStructureNode child : node.getChildren()) {
+				for (int i = 0; i < nextLevel; ++i) {
+					System.out.print('\t');
+				}
+				System.out.println(child);
+				print(child, true, nextLevel);
+			}
+		} else {
+			for (IStructureNode child : node.getChildren()) {
+				print(child, false, level);
+			}
+		}
 	}
 
 }
