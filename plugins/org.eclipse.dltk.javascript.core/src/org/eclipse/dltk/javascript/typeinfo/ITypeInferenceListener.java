c23969a6cf5975a9fdbc8651e436f9ed89436a38
Alex Panchenko <alex.panchenko@gmail.com>
2013-02-13 17:06:12 +0700

    javadocs for ITypeInferenceListener

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/typeinfo/ITypeInferenceListener.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/typeinfo/ITypeInferenceListener.java
@@ -14,12 +14,26 @@ package org.eclipse.dltk.javascript.typeinfo;
 import org.eclipse.dltk.javascript.typeinfo.IModelBuilder.IMethod;
 import org.eclipse.dltk.javascript.typeinfo.IModelBuilder.IVariable;
 
+/**
+ * Listener for events happening during type inferencing.
+ * 
+ * @see ITypeInferencerVisitor#addListener(ITypeInferenceListener)
+ */
 public interface ITypeInferenceListener {
 
+	/**
+	 * Notifies that variable declaration was parsed.
+	 */
 	void variableParsed(IVariable variable);
 
+	/**
+	 * Notifies that method declaration was parsed.
+	 */
 	void methodParsed(IMethod method);
 
+	/**
+	 * Notifies that type inferencing of the file was completed.
+	 */
 	void done();
 
 }
