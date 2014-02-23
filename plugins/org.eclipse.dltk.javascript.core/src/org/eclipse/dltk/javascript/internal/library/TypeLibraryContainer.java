0811efc6eb54c3a87dc8e1fe9ec2f4323423eaeb
Alex Panchenko <alex.panchenko@gmail.com>
2014-02-23 14:45:07 +0100

    validation if type library not found

--- plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/internal/library/TypeLibraryContainer.java
+++ plugins/org.eclipse.dltk.javascript.core/src/org/eclipse/dltk/javascript/internal/library/TypeLibraryContainer.java
@@ -16,13 +16,20 @@ import static org.eclipse.dltk.core.environment.EnvironmentManager.getLocalEnvir
 import static org.eclipse.dltk.core.environment.EnvironmentPathUtils.getFullPath;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.dltk.core.IBuildpathContainer;
+import org.eclipse.dltk.core.IBuildpathContainerExtension2;
 import org.eclipse.dltk.core.IBuildpathEntry;
+import org.eclipse.dltk.core.IModelStatus;
+import org.eclipse.dltk.javascript.core.JavaScriptPlugin;
 import org.eclipse.dltk.javascript.typeinfo.TypeLibrary;
 import org.eclipse.dltk.javascript.typeinfo.TypeLibraryManager;
+import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.Version;
 
-public class TypeLibraryContainer implements IBuildpathContainer {
+public class TypeLibraryContainer implements IBuildpathContainer,
+		IBuildpathContainerExtension2 {
 
 	public static final String ID = "org.eclipse.dltk.javascript.TypeLibrary";
 
@@ -77,5 +84,52 @@ public class TypeLibraryContainer implements IBuildpathContainer {
 
 	public IPath getPath() {
 		return containerPath;
+	}
+
+	static class ContainerStatus extends Status implements IModelStatus {
+
+		private final IPath path;
+
+		public ContainerStatus(int severity, String message, IPath path) {
+			super(severity, JavaScriptPlugin.PLUGIN_ID, message);
+			this.path = path;
+		}
+
+		@Override
+		public boolean isDoesNotExist() {
+			return false;
+		}
+
+		@Override
+		public IPath getPath() {
+			return path;
+		}
+	}
+
+	@Override
+	public IModelStatus validate() {
+		Version version = null;
+		IllegalArgumentException versionException = null;
+		if (containerPath.segmentCount() > 2) {
+			try {
+				version = Version.parseVersion(containerPath.segment(2));
+			} catch (IllegalArgumentException e) {
+				versionException = e;
+			}
+		}
+		final TypeLibrary library = TypeLibraryManager.getManager().find(
+				containerPath.segment(1), version);
+		if (library == null) {
+			return new ContainerStatus(IStatus.ERROR, NLS.bind(
+					"Type Library {0} not found", containerPath.segment(1)),
+					containerPath);
+		}
+		if (versionException != null) {
+			return new ContainerStatus(IStatus.ERROR, NLS.bind(
+					"Error parsing {0} type library version {1}",
+					containerPath.segment(1), containerPath.segment(2)),
+					containerPath);
+		}
+		return null;
 	}
 }
